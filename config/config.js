53c554d017b0116b507a43217dadf4135d18da3b
ronadniho <463329429@qq.com>
2018-07-13 10:28:42 +0800

    first

---
+++ config/config.js
@@ -0,0 +1,10 @@
+exports.config =  {
+  "testUrl": "https://test.e-celap.com",
+  "SWYtestUrl":"https://test.e-celap.com",
+  "ZWYproductUrl":"https://test.e-celap.com/appzwy",//打包测试环境
+  // "SWYproductUrl":"https://test.e-celap.com/swyapp",//打包测试环境
+  "SWYproductUrl":"http://swy.shgb.gov.cn",//打包正式环境
+  // "ZWYproductUrl":"https://cela.e-celap.com",
+  "imgUrl":"https://file.e-celap.com/FileDownload"
+
+}
